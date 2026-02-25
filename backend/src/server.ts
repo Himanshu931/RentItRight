be0b327ab2304f0d7df704838399ec0e0f447daf
RahulSingh044 <rahulsingh.dev.36@gmail.com>
2026-02-25 14:15:02 +0530

    fix: changes in backend acc to new address schema changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import app from "./app";
 import { connectDB } from "./config/db";
 import logger from "./config/logger";
-const PORT = 3000;
+const PORT = "10.93.208.201";
 
 connectDB();
 
 app.listen(PORT, () => {
-    logger.info(`Server running on http://localhost:${PORT}`);
+    logger.info(`Server running on http://${PORT}`);
 });
--- backend/src/validatior/user.schema.ts
+++ backend/src/validatior/user.schema.ts
@@ -7,9 +7,8 @@ export const profileSchema = z.object({
 
     address: z.object({
         pincode: z.string().min(6, "Pincode must be 6 digits"),
-        city: z.string().min(2, "City is required"),
+        district: z.string().min(2, "District is required"),
         state: z.string().min(2, "State is required"),
-        country: z.string().min(2, "Country is required"),
     }),
 
     roles: z.enum(["renter", "owner", "admin"]).default("renter"),
@@ -21,7 +20,6 @@ export const updateProfileSchema = profileSchema.partial();
 
 export const updateAddressSchema = z.object({
     pincode: z.string().min(6, "Pincode must be 6 digits").optional(),
-    city: z.string().min(2, "City is required").optional(),
+    district: z.string().min(2, "District is required").optional(),
     state: z.string().min(2, "State is required").optional(),
-    country: z.string().min(2, "Country is required").optional(),
 })
